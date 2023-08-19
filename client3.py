abe0ab2ea8a926fc6e9074a94fae0cea7b776d4b
Nazim Kokcu <mnkokcu@ncsu.edu>
2023-08-19 16:40:44 -0400

    Task1 + Task1 Bonus Material Completed

--- client3.py
+++ client3.py
@@ -28,32 +28,44 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-
+#In this method, I've made modifications to compute the right stock price. This means I changed how
+#`price` is computed by using the formula: (bid_price+ask_price) / 2. 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2.0
     return stock, bid_price, ask_price, price
 
+#change the return value to the ratio of stock price_a to stock price_b. Also there is one error checking
+#if statement added in case of the price_b is 0.
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if(price_b ==0):
+        return 0
+    
+    return price_a/price_b
 
 
-# Main
+
+# Main: Now that the two other methods fixed, just need to print the correct values. For every iteration in the
+# main method, I stored the datapoints that I got from the getDataPoint method so that I can properly call
+# getRatio and print the right ratio.
+
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
--- client_test.py
+++ client_test.py
@@ -2,24 +2,42 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+        # Calculate the expected data points based on the provided quotes
+        expected_data_points = [
+            ('ABC', 120.48, 121.2, 120.84),
+            ('DEF', 117.87, 121.68, 119.775)
+        ]
 
+        for i, quote in enumerate(quotes):
+            with self.subTest(i=i):
+                actual_data_point = getDataPoint(quote)
+                self.assertEqual(actual_data_point, expected_data_points[i])
 
-  """ ------------ Add more unit tests ------------ """
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
 
+        # Calculate the expected data points based on the provided quotes
+        expected_data_points = [
+            ('ABC', 120.48, 119.2, 119.84),
+            ('DEF', 117.87, 121.68, 119.775)
+        ]
 
+        for i, quote in enumerate(quotes):
+            with self.subTest(i=i):
+                actual_data_point = getDataPoint(quote)
+                self.assertEqual(actual_data_point, expected_data_points[i])
+
+    """ ------------ Add more unit tests ------------ """
 
 if __name__ == '__main__':
     unittest.main()
+
